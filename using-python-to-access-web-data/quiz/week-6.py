dc08ab6e6384a6c84969707d938de0bb797d5a68
Silly Script <70499403+sillyscript@users.noreply.github.com>
2024-10-15 10:01:44 +0530

    Create week-6.py

---
+++ using-python-to-access-web-data/quiz/week-6.py
@@ -0,0 +1,41 @@
+__author__ = "Prayas"
+
+
+1. Who is credited with getting the JSON movement started?
+Ans: Douglas Crockford
+
+
+2. Which of the following is true about an API?
+Ans: An API is a contract that defines how to use a software library
+
+
+3. Which of the following is a web services approach used by the Twitter API?
+Ans: REST
+
+
+4. What kind of variable will you get in Python when the following JSON is parsed:
+Ans: A dictionary with three key / value pairs
+
+
+5. Which of the following is not true about the service-oriented approach?
+Ans: An application runs together all in one place
+
+
+6. If the following JSON were parsed and put into the variable x, what Python code would extract "Leah Culver" from the JSON?
+Ans: x["users"][0]["name"]
+
+
+7. What library call do you make to append properly encoded parameters to the end of a URL like the following:
+Ans: urllib.parse.urlencode()
+
+
+8. What happens when you exceed the Google geocoding API rate limit?
+Ans: You cannot use the API for 24 hours
+
+
+9. What protocol does Twitter use to protect its API?
+Ans: OAuth
+
+
+10. What header does Twitter use to tell you how many more API requests you can make before you will be rate limited?
+Ans: x-rate-limit-remaining

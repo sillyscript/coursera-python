88f7dbc3999b68387445c5f74cea4c6b43ef2484
Silly Script <70499403+sillyscript@users.noreply.github.com>
2024-10-15 09:55:56 +0530

    Create week-3.py

---
+++ using-python-to-access-web-data/quiz/week-3.py
@@ -0,0 +1,46 @@
+__author__ = "Prayas"
+
+
+1. What do we call it when a browser uses the HTTP protocol to load a file or page from a server and display it in the browser?
+Ans: The Request/Response Cycle
+
+
+2. Which of the following is most similar to a TCP port number?
+Ans: A telephone extension
+
+
+3. What must you do in Python before opening a socket?
+Ans: import socket
+
+
+4. Which of the following TCP sockets is most commonly used for the web protocol (HTTP)?
+Ans: 80
+
+                                                           
+5. Which of the following is most like an open socket in an application?
+Ans: An "in-progress" phone conversation
+
+
+6. What does the "H" of HTTP stand for?
+Ans: HyperText
+
+
+7. What is an important aspect of an Application Layer protocol like HTTP?
+Ans: Which application talks first? The client or server?
+
+
+8. What are the three parts of this URL (Uniform Resource Locator)?
+
+    http://www.dr-chuck.com/page1.htm
+
+Ans: Protocol, host, and document
+
+
+9. When you click on an anchor tag in a web page like below, what HTTP request is sent to the server?
+
+Please click here.
+Ans: GET
+
+
+10. Which organization publishes Internet Protocol Standards?
+Ans: IETF

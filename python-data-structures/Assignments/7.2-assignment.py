7edd2efcce751df561ca0372be8a9f2967751fcc
Silly Script <70499403+sillyscript@users.noreply.github.com>
2024-10-15 15:17:21 +0530

    Update 7.2-assignment.py

--- python-data-structures/Assignments/7.2-assignment.py
+++ python-data-structures/Assignments/7.2-assignment.py
@@ -1,22 +1,28 @@
 __author__ = "Prayas"
 
-if __name__ == '__main__':
-    fname = input("Enter file name: ")
-    fh = open(fname)
-    s = 0.0
-    con = 0
+fname = input("Enter file name: ")
 
-    for line in fh:
-        if not line.startswith("X-DSPAM-Confidence:"):
-            continue
-        else:
-            try:
-                l = len(line)
-                temp = line.find(':')
-                v = float(line[temp + 1:l])
-                s  = s + v
-                con += 1
-            except:
-                pass
+try:
+    fhandle = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    quit()
 
-    print("Average spam confidence: %0.12f" % (s / con))
+count = 0
+total = 0
+
+for line in fhandle:
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+
+    pos = line.find(':')
+    value = float(line[pos+1:].strip())
+    
+    total += value
+    count += 1
+
+if count > 0:
+    average = total / count
+    print("Average spam confidence:", average)
+else:
+    print("No relevant lines found.")

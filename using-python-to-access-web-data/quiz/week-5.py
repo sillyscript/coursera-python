c0e0a859e7435ac9dff234c1fea771fa4887962c
Silly Script <70499403+sillyscript@users.noreply.github.com>
2024-10-15 10:00:22 +0530

    Create week-5.py

---
+++ using-python-to-access-web-data/quiz/week-5.py
@@ -0,0 +1,53 @@
+__author__ = "Prayas"
+
+
+1. What is the name of the Python 2.x library to parse XML data?
+Ans: xml.etree.ElementTree
+
+
+2. Which of the following are not commonly used serialization formats?
+Ans:
+
+    Dictionaries
+    HTTP
+    TCP
+
+
+3. In this XML, which are the "simple elements"?
+Ans:
+
+    name
+    phone
+
+
+4. In the following XML, which are attributes?
+Ans:
+
+    type
+    hide
+
+
+5. In the following XML, which node is the parent node of node e
+Ans: c
+
+
+6. Looking at the following XML, what text value would we find at path "/a/c/e"
+Ans: Z
+
+
+7. What is the purpose of XML Schema?
+Ans: To establish a contract as to what is valid XML
+
+
+8. For this XML Schema:
+And this XML,
+Which tag is incorrect?
+Ans: Age
+
+
+9. What is a good time zone to use when computers are exchanging data over APIs?
+Ans: Universal Time / GMT
+
+
+10. Which of the following dates is in ISO8601 format?
+Ans: 2002-05-30T09:30:10Z

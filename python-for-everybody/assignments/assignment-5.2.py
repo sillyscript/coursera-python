3ade8028803b6e2c615fd661e96f548902c9b19b
Silly Script <70499403+sillyscript@users.noreply.github.com>
2024-10-20 19:49:51 +0530

    Update assignment-5.2.py

--- python-for-everybody/assignments/assignment-5.2.py
+++ python-for-everybody/assignments/assignment-5.2.py
@@ -3,29 +3,27 @@ Once 'done' is entered, print out the largest and smallest of the numbers.
 If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number.
 Enter the numbers from the book for problem 5.1 and Match the desired output as shown.
 
+
 largest = None
 smallest = None
+
 while True:
-        num = raw_input("Enter a number: ")
-if num == "done" : break
+    num = input("Enter a number: ")
+    
+    if num == "done":
+        break
 
-
-try:
+    try:
         num = int(num)
-except:
+    except:
         print("Invalid input")
-continue
+        continue
 
-if largest is None:
+    if largest is None or num > largest:
         largest = num
-elif largest < num:
-        largest = num
-
-if smallest is None:
+    
+    if smallest is None or num < smallest:
         smallest = num
-elif smallest > num:
-        smallest = num
-
 
 print("Maximum is", largest)
 print("Minimum is", smallest)

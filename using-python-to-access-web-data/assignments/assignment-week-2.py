7e3bd44cdae155543b62937beada2a5338267fca
Silly Script <70499403+sillyscript@users.noreply.github.com>
2024-10-26 19:37:09 +0530

    Update assignment-week-2.py

--- using-python-to-access-web-data/assignments/assignment-week-2.py
+++ using-python-to-access-web-data/assignments/assignment-week-2.py
@@ -3,8 +3,8 @@ __author__ = "Prayas"
 import re
 
 #check file name before execution it could be 2091693
-#file_name = "regex_sum_2110012.txt"
-file_name = "regex_sum_2091693.txt" 
+file_name = "regex_sum_2110012.txt"
+#file_name = "regex_sum_2091693.txt" 
 with open(file_name, 'r') as file:
     text = file.read()
 
@@ -13,3 +13,6 @@ numbers = re.findall(r'[0-9]+', text)
 total_sum = sum(int(num) for num in numbers)
 
 print("Sum:", total_sum)
+
+#For 2091693 sum is 358113
+#For 2110012 sum is 
